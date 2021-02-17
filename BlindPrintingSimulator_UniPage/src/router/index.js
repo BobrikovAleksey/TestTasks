fae57c61ae37c980a74e9a5c9c35e888b3573852
BobrikovAleksey <bobrikov.spb@ya.ru>
2021-02-17 15:31:22 +0300

    Blind printing simulator: fixed path for dist   (#19)

--- BlindPrintingSimulator_UniPage/src/router/index.js
+++ BlindPrintingSimulator_UniPage/src/router/index.js
@@ -15,6 +15,10 @@ const routes = [
     component: Testing,
     meta: { scrollToTop: true },
   },
+  {
+    path: '/TestTasks/BlindPrintingSimulator_UniPage/dist',
+    redirect: '/',
+  },
 ];
 
 const router = createRouter({
